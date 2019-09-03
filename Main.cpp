12964205aa9d71872152ad3d90aa6f92e0bc81c6
SKNJPN <sknjpn@gmail.com>
2019-09-03 10:52:02 +0900

    サンプルの変更

--- Main.cpp
+++ Main.cpp
@@ -16,35 +16,38 @@ void Main()
 	// 猫の座標
 	Vec2 catPos(640, 450);
 
-	CursorCamera camera;
+	RestrictedCamera2D camera;
 
 	while (System::Update())
 	{
 		camera.update();
-
-		auto t = camera.createTransformer();
-
-		// テキストを画面の中心に描く
-		font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
-
-		// 大きさをアニメーションさせて猫を表示する
-		cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
-
-		// マウスカーソルに追従する半透明の赤い円を描く
-		Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
-
-		// [A] キーが押されたら
-		if (KeyA.down())
+		camera.getRestrictedRect().drawFrame(1.0, Palette::Red);
+		
 		{
-			// Hello とデバッグ表示する
-			Print << U"Hello!";
-		}
+			auto t = camera.createTransformer();
 
-		// ボタンが押されたら
-		if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
-		{
-			// 猫の座標を画面内のランダムな位置に移動する
-			catPos = RandomVec2(Scene::Rect());
+			// テキストを画面の中心に描く
+			font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
+
+			// 大きさをアニメーションさせて猫を表示する
+			cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
+
+			// マウスカーソルに追従する半透明の赤い円を描く
+			Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
+
+			// [A] キーが押されたら
+			if (KeyA.down())
+			{
+				// Hello とデバッグ表示する
+				Print << U"Hello!";
+			}
+
+			// ボタンが押されたら
+			if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
+			{
+				// 猫の座標を画面内のランダムな位置に移動する
+				catPos = RandomVec2(Scene::Rect());
+			}
 		}
 	}
 }
